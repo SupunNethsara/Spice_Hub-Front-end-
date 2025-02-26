4f330e87338f51f2e55e1ca9633182c138282725
Supun Nethsara <supunmax663@gmail.com>
2025-02-26 00:43:19 -0800

    Product Section Created

--- src/Components/Othercomponents/Products/Products.jsx
+++ src/Components/Othercomponents/Products/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Products.scss'
 import Product_head from './Product_head'
+import Products_Cart from './Products_Cart'
 export default function Products() {
   return (
     <div>
       <Product_head/>
+      <Products_Cart/>
     </div>
   )
 }
